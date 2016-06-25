28e50fcbf5d38a1ec3989dc4a440f525d41c5d9b
Jakub Pelczar <jakubpelczar92@gmail.com>
2016-06-25 21:05:05 +0200

    Init commit

---
+++ Application/src/main/java/pl/jpelczar/pos_app/Main.java
@@ -0,0 +1,9 @@
+package pl.jpelczar.pos_app;
+
+/**
+ * Created by jpelczar on 25.06.16.
+ */
+public class Main {
+    public static void main(String[] argv) {
+    }
+}
---
+++ Framework/src/main/java/pl/jpelczar/pos_framework/AAA.java
@@ -0,0 +1,7 @@
+package pl.jpelczar.pos_framework;
+
+/**
+ * Created by jpelczar on 25.06.16.
+ */
+public class AAA {
+}
