bcedad95cfc4e6413a5e6b424b1ab486003bd8df
Jakub Pelczar <jakubpelczar92@gmail.com>
2016-06-26 23:12:26 +0200

    Add main window to app

--- Application/src/main/java/pl/jpelczar/pos_app/Main.java
+++ Application/src/main/java/pl/jpelczar/pos_app/Main.java
@@ -1,9 +1,13 @@
 package pl.jpelczar.pos_app;
 
+import pl.jpelczar.pos_app.ui.MainUI;
+
 /**
  * Created by jpelczar on 25.06.16.
  */
 public class Main {
     public static void main(String[] argv) {
+        MainUI mainUi = new MainUI();
+        mainUi.setVisible(true);
     }
 }
---
+++ Application/src/main/java/pl/jpelczar/pos_app/ui/MainUI.java
@@ -0,0 +1,16 @@
+package pl.jpelczar.pos_app.ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by jpelczar on 26.06.16.
+ */
+public class MainUI extends JFrame {
+
+    public MainUI() throws HeadlessException {
+        super("Point of Sale");
+        setSize(400, 400);
+        setResizable(false);
+    }
+}
