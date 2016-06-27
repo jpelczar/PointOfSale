b25cfd89ae9c3fbc4dd7d4401b2a5e556deb9573
Jakub Pelczar <jakubpelczar92@gmail.com>
2016-06-28 00:41:08 +0200

    Add javadoc

--- Framework/src/main/java/pl/jpelczar/pos_framework/PointOfSaleAPI.java
+++ Framework/src/main/java/pl/jpelczar/pos_framework/PointOfSaleAPI.java
@@ -10,18 +10,42 @@ import java.util.List;
  */
 public interface PointOfSaleAPI {
 
+    /**
+     * Method for invoke barcode scan
+     *
+     * @param callback - invoke when scanner end work
+     */
     void scan(ScanCallback callback);
 
 //    void confirmScannedProduct(Product product, int count); //have only one input device - barcode scanner
 
+    /**
+     * Method for begin new receipt
+     */
     void startReceipt();
 
+    /**
+     * End current receipt. E.g save to database.
+     *
+     * @return current closed receipt
+     */
     Receipt closeReceipt();
 
+    /**
+     * @return all persisted products
+     */
     List<Product> getProducts();
 
+    /**
+     * @return current receipt
+     */
     Receipt getReceipt();
 
+    /**
+     * Add product int database, storage
+     *
+     * @param product
+     */
     void addProduct(Product product);
 
 }
